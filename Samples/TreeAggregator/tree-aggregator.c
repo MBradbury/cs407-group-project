b851afa3044beb7e07dd37c84775cae251985e24
MBradbury <M.Bradbury@warwick.ac.uk>
2012-10-27 18:16:18 +0200

    More work on tree aggregator

--- Samples/TreeAggregator/tree-aggregator.c
+++ Samples/TreeAggregator/tree-aggregator.c
@@ -6,6 +6,9 @@
 #include "lib/sensors.h"
 #include "dev/sht11.h"
 #include "dev/sht11-sensor.h"
+#include "dev/battery-sensor.h"
+
+#include "dev/leds.h"
 
 #include "net/netstack.h"
 #include "net/rime.h"
@@ -13,7 +16,7 @@
 #include "net/rime/timesynch.h"
 #include "contiki-net.h"
 
-double sht11_relative_humidity(unsigned raw)
+static double sht11_relative_humidity(unsigned raw)
 {
 	// FROM:
 	// http://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/Humidity/Sensirion_Humidity_SHT1x_Datasheet_V5.pdf
@@ -32,7 +35,7 @@ double sht11_relative_humidity(unsigned raw)
 	return c1 + c2 * raw + c3 * raw * raw;
 }
 
-double sht11_relative_humidity_compensated(unsigned raw, double temperature)
+static double sht11_relative_humidity_compensated(unsigned raw, double temperature)
 {
 	// 12-bit
 	static const double t1 = 0.01;
@@ -48,7 +51,7 @@ double sht11_relative_humidity_compensated(unsigned raw, double temperature)
 }
 
 /** Output temperature in degrees Celcius */
-double sht11_temperature(unsigned raw)
+static double sht11_temperature(unsigned raw)
 {
 	//static const double d1 = -40.1; // 5V
 	//static const double d1 = -39.8; // 4V
@@ -60,6 +63,12 @@ double sht11_temperature(unsigned raw)
 	//static const double d2 = 0.04; // 12-bit
 
 	return d1 + d2 * raw;
+}
+
+/** From: www.scribd.com/doc/73156710/Contiki-1 */
+static double battery_voltage(unsigned raw)
+{
+	return (raw * 2.500 * 2.0) / 4096;
 }
 
 
@@ -89,6 +98,7 @@ static const char * message_type_to_string(message_type_t type)
 typedef struct
 {
 	message_type_t type;
+	rimeaddr_t source;
 } base_msg_t;
 
 /** The structure of the message we are sending */
@@ -125,20 +135,30 @@ static void parent_detect_finished(void * ptr)
 {
 	struct broadcast_conn * conn = (struct broadcast_conn *)ptr;
 
-	printf("Timer on %d.%d expired\n", rimeaddr_node_addr.u8[0], rimeaddr_node_addr.u8[1]);
+	// As we are no longer listening for our parent node
+	// indicate so through the LEDs
+	leds_off(LEDS_RED);
+
+	printf("Timer on %d.%d expired\n",
+		rimeaddr_node_addr.u8[0], rimeaddr_node_addr.u8[1]);
 
 	// Set the best values
 	best_parent = collecting_best_parent;
 	best_hop = collecting_best_hop;
 
-	printf("Found: Parent:%d.%d Hop:%u\n", best_parent.u8[0], best_parent.u8[1], best_hop);
+	printf("Found: Parent:%d.%d Hop:%u\n",
+		best_parent.u8[0], best_parent.u8[1], best_hop);
 
+	// Send a message that is to be received by the children
+	// of this node.
 	setup_tree_msg_t * nextmsg;
 
 	packetbuf_clear();
 	packetbuf_set_datalen(sizeof(setup_tree_msg_t));
 	nextmsg = (setup_tree_msg_t *)packetbuf_dataptr();
 
+	// We set the parent of this node to be the best
+	// parent we heard
 	nextmsg->base.type = setup_message_type;
 	rimeaddr_copy(&nextmsg->id, &rimeaddr_node_addr);
 	rimeaddr_copy(&nextmsg->parent, &best_parent);
@@ -146,8 +166,11 @@ static void parent_detect_finished(void * ptr)
 
 	broadcast_send(conn);
 
+	// We are done with setting up the tree
+	// so stop listening for setup messages
 	broadcast_close(conn);
 
+	// Start the data generation process
 	process_start(&send_data_process, NULL);
 }
 
@@ -165,8 +188,9 @@ static void recv_aggregate(struct broadcast_conn * ptr, rimeaddr_t const * origi
 			// If the destination is this node
 			if (rimeaddr_cmp(&msg->destination, &rimeaddr_node_addr) != 0)
 			{
-				printf("Network Data: Addr:%d.%d Temp:%d Hudmid:%d%%\n",
+				printf("Network Data: Addr:%d.%d Src:%d.%d Temp:%d Hudmid:%d%%\n",
 					originator->u8[0], originator->u8[1],
+					msg->base.source.u8[0], msg->base.source.u8[1],
 					(int)msg->temperature, (int)msg->humidity
 				);
 
@@ -174,11 +198,13 @@ static void recv_aggregate(struct broadcast_conn * ptr, rimeaddr_t const * origi
 				// Apply some aggregation function
 
 			
-				// Forward message onwards
-				msg->base.type = collect_message_type;
-				rimeaddr_copy(&msg->destination, &best_parent);
+				// Forward message onwards, if not the sink
+				if (!is_sink())
+				{
+					rimeaddr_copy(&msg->destination, &best_parent);
 		
-				broadcast_send(ptr);
+					broadcast_send(ptr);
+				}
 			}
 		} break;
 
@@ -202,21 +228,54 @@ static void recv_setup(struct broadcast_conn * ptr, rimeaddr_t const * originato
 
 			printf("Got setup message from %d.%d\n", originator->u8[0], originator->u8[1]);
 
+			// If the sink received a setup message, then do nothing
+			// it doesn't need a parent as it is the root.
+			if (is_sink())
+			{
+				printf("We are the sink node, so should not listen for parents.\n");
+				break;
+			}
+
+			// If this is the first setup message that we have seen
+			// Then we need to start the collect timeout
 			if (!has_seen_setup)
 			{
+				unsigned battery_raw;
+
 				has_seen_setup = true;
+
+				// Indicate that we are setting up
+				leds_on(LEDS_RED);
+
+
+				// TODO: In the future the time to wait for
+				// parents should be related to the battery remaining.
+				// The more power remaining, the more time should be
+				// spent listening
+				SENSORS_ACTIVATE(battery_sensor);
+				battery_raw = battery_sensor.value(0);
+				SENSORS_DEACTIVATE(battery_sensor);
+
+
+				printf("Battery value Raw:%u Real:%dV\n", battery_raw, (int)battery_voltage(battery_raw));
+
+				// Start the timer that will call a function when we are
+				// done detecting parents.
 				ctimer_set(&ct, 15 * CLOCK_SECOND, &parent_detect_finished, ptr);
 
 				printf("Not seen setup message before, so setting timer...\n");
 			}
 
+			// As we have received a message we need to record the node
+			// it came from, if it is closer to the sink.
 			if (msg->hop_count < best_hop)
 			{
 				// Set the best parent, and the hop count of that node
 				rimeaddr_copy(&collecting_best_parent, &msg->id);
 				collecting_best_hop = msg->hop_count;
 
-				printf("Updating to a better parent (%d.%d)\n", collecting_best_parent.u8[0], collecting_best_parent.u8[1]);
+				printf("Updating to a better parent (%d.%d)\n",
+					collecting_best_parent.u8[0], collecting_best_parent.u8[1]);
 			}
 		} break;
 
@@ -235,28 +294,52 @@ AUTOSTART_PROCESSES(&tree_setup_process);
 
 PROCESS_THREAD(tree_setup_process, ev, data)
 {
+	static struct etimer et;
 	static struct broadcast_conn bc;
+	static int i;
 
 	PROCESS_BEGIN();
 
-	broadcast_open(&bc, 128, &callbacks_setup);
-
 	if (is_sink())
 	{
-		setup_tree_msg_t * msg;
+		// Sink only needs to listen for aggregated messages
+		broadcast_open(&bc, 128, &callbacks_aggregate);
 
-		packetbuf_clear();
-		packetbuf_set_datalen(sizeof(setup_tree_msg_t));
-		msg = (setup_tree_msg_t *)packetbuf_dataptr();
+		leds_on(LEDS_YELLOW);
 
-		msg->base.type = setup_message_type;
-		rimeaddr_copy(&msg->id, &rimeaddr_node_addr);
-		rimeaddr_copy(&msg->parent, &rimeaddr_null);
-		msg->hop_count = 0;
+		etimer_set(&et, 4 * CLOCK_SECOND);
+
+		// Retry initial send a few times to make sure
+		// that the surrounding nodes will get a message
+		for (i = 0; i != 4; ++i)
+		{
+			PROCESS_YIELD();
+
+			setup_tree_msg_t * msg;
+
+			// Send the first message that will be used to set up the
+			// aggregation tree
+			packetbuf_clear();
+			packetbuf_set_datalen(sizeof(setup_tree_msg_t));
+			msg = (setup_tree_msg_t *)packetbuf_dataptr();
+
+			msg->base.type = setup_message_type;
+			rimeaddr_copy(&msg->base.source, &rimeaddr_node_addr);
+			rimeaddr_copy(&msg->id, &rimeaddr_node_addr);
+			rimeaddr_copy(&msg->parent, &rimeaddr_null);
+			msg->hop_count = 0;
 	
-		broadcast_send(&bc);
+			broadcast_send(&bc);
 
-		printf("IsSink, sending initial message...\n");
+			printf("IsSink, sending initial message (%d)...\n", i);
+
+			etimer_reset(&et);
+		}
+	}
+	else
+	{
+		// Other nodes need to listen for setup messages
+		broadcast_open(&bc, 128, &callbacks_setup);
 	}
 
 	PROCESS_END();
@@ -273,13 +356,15 @@ PROCESS_THREAD(send_data_process, ev, data)
 	PROCESS_EXITHANDLER(goto exit;)
 	PROCESS_BEGIN();
 
+	leds_on(LEDS_GREEN);
+
 	broadcast_open(&bc, 128, &callbacks_aggregate);
 
 	// By this point the tree should be set up,
 	// so now we should move to aggregating data
 	// through the tree
 
-	etimer_set(&et, 3 * CLOCK_SECOND);
+	etimer_set(&et, 20 * CLOCK_SECOND);
  
 	while (true)
 	{
@@ -301,6 +386,7 @@ PROCESS_THREAD(send_data_process, ev, data)
 		msg = (collect_msg_t *)packetbuf_dataptr();
 
 		msg->base.type = collect_message_type;
+		rimeaddr_copy(&msg->base.source, &rimeaddr_node_addr);
 		rimeaddr_copy(&msg->destination, &best_parent);
 		msg->temperature = sht11_temperature(raw_temperature);
 		msg->humidity = sht11_relative_humidity_compensated(raw_humidity, msg->temperature);

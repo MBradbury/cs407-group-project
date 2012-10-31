ec076b002901cbc2960aa98ac17b5e5be6cceec3
Amit Shah <amitshah@oneuk.com>
2012-10-31 16:51:03 +0000

    Can handle multiple nodes, only a max of 8 Message recieved broadcasts sent)

--- Algorithms/hsend/Node.c
+++ Algorithms/hsend/Node.c
@@ -16,12 +16,21 @@
 #include "net/rime/timesynch.h"
 #include "contiki-net.h"
 
+
+
 /** The function that will be executed when a message is received */
 static void recv(rimeaddr_t const * originator, uint8_t seqno, uint8_t hops)
 {
 	//printf("Message received\n");
 
-	printf("%s\n",(char *)packetbuf_dataptr() );
+	printf("Message: %s\n",(char *)packetbuf_dataptr() );
+
+	static int count = 0;
+
+	if(count++ <= 8){
+				sendRIMEMessage("Message received");
+	}
+
 }	
 
 /** List of all functions to execute when a message is received */
@@ -33,29 +42,30 @@ static const int REXMITS = 4;
 PROCESS(messageSenderProcess, "Message Sender");
 AUTOSTART_PROCESSES(&messageSenderProcess);
 
-PROCESS_THREAD(messageSenderProcess, ev, data)
+void sendRIMEMessage(char * message)
 {
-	PROCESS_EXITHANDLER(goto exit;)
-	PROCESS_BEGIN();
-	
-
-	while(1)
-	{
-		//printf("Hello World\n");
 		collect_open(&tc, 128, COLLECT_ROUTER, &callbacks);
 	
-	if (rimeaddr_node_addr.u8[0] == 1 &&
+	/*if (rimeaddr_node_addr.u8[0] == 1 &&
 		rimeaddr_node_addr.u8[1] == 0)
-	{
-		char * msg = "Hello World!";
-
+	{*/
 		collect_set_sink(&tc, 1);
 	
 		//packetbuf_clear();
-		packetbuf_copyfrom(msg, 6);
+		packetbuf_copyfrom(message, strlen(message));
 		collect_send(&tc, REXMITS);
-	}
-	}
-exit:
-	collect_close(&tc);
-	PROCESS_END();}
+		collect_close(&tc);
+	//}
+
+}
+
+
+PROCESS_THREAD(messageSenderProcess, ev, data)
+{
+	PROCESS_BEGIN();
+	printf("Sending initial message\n");
+	sendRIMEMessage("Hello World again!");
+
+	PROCESS_END();
+}
+

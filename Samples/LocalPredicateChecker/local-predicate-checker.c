506c1cea54b67111515689eea22f21ea56ee46b9
MBradbury <M.Bradbury@warwick.ac.uk>
2012-11-01 21:04:10 +0100

    Improve local predicate checking, by reporting the error message back to the sink

--- Samples/LocalPredicateChecker/local-predicate-checker.c
+++ Samples/LocalPredicateChecker/local-predicate-checker.c
@@ -20,10 +20,62 @@
 #include "contiki-net.h"
 
 
+static struct collect_conn tc;
+
+// The number of retransmits to perform
+static const int NORMAL_REXMITS = 4;
+static const int ERROR_REXMITS = 10;
+
+#define ERROR_MESSAGE_MAX_LENGTH 128
+
+
+
+typedef enum
+{
+	collect_message_type,
+	error_message_type
+} message_type_t;
+
+static const char * message_type_to_string(message_type_t type)
+{
+	switch (type)
+	{
+		case collect_message_type: return "Collect Message";
+		case error_message_type: return "Error Message";
+		default: return "Unknown Message";
+	}
+}
+
+typedef struct
+{
+	message_type_t type;
+} base_msg_t;
+
+/** The structure of the message we are sending */
+typedef struct
+{
+	base_msg_t base;
+
+	double temperature;
+	double humidity;
+
+} collect_msg_t;
+
+typedef struct
+{
+	base_msg_t base;
+
+	char contents[ERROR_MESSAGE_MAX_LENGTH + 1];
+	size_t length;
+
+} error_msg_t;
+
+
+
 typedef bool (*predicate_checker_t)(void const *);
 typedef void (*predicate_failure_message_t)(void const *);
 
-void check_predicate(
+static void check_predicate(
 	predicate_checker_t predicate,
 	predicate_failure_message_t message,
 	void const * state)
@@ -38,36 +90,52 @@ void check_predicate(
 	}
 }
 
-bool temperature_validator(void const * value)
+static bool temperature_validator(void const * value)
 {
 	double temperature = *(double const *)value;
 
 	return temperature > 0 && temperature <= 40;
 }
 
-void temperature_message(void const * value)
+static void temperature_message(void const * value)
 {
 	double temperature = *(double const *)value;
 
-	printf("Temperature predicate check failed %d\n", (int)temperature);
+	packetbuf_clear();
+	packetbuf_set_datalen(sizeof(error_msg_t));
+	error_msg_t * msg = (error_msg_t *)packetbuf_dataptr();
+
+	msg->base.type = error_message_type;
+
+	msg->length = snprintf(msg->contents, ERROR_MESSAGE_MAX_LENGTH, "Temperature predicate check failed (0 < T <= 40) where T=%d\n", (int)temperature);
+	
+	collect_send(&tc, ERROR_REXMITS);
 }
 
-bool humidity_validator(void const * value)
+static bool humidity_validator(void const * value)
 {
 	double humidity = *(double const *)value;
 
 	return humidity > 0 && humidity <= 100;
 }
 
-void humidity_message(void const * value)
+static void humidity_message(void const * value)
 {
 	double humidity = *(double const *)value;
 
-	printf("Humidity predicate check failed: %d%%\n", (int)humidity);
+	packetbuf_clear();
+	packetbuf_set_datalen(sizeof(error_msg_t));
+	error_msg_t * msg = (error_msg_t *)packetbuf_dataptr();
+
+	msg->base.type = error_message_type;
+
+	msg->length = snprintf(msg->contents, ERROR_MESSAGE_MAX_LENGTH, "Humidity predicate check failed (0 < H <= 100) where H=%d%%\n", (int)humidity);
+	
+	collect_send(&tc, ERROR_REXMITS);
 }
 
 
-double sht11_relative_humidity(unsigned raw)
+static double sht11_relative_humidity(unsigned raw)
 {
 	// FROM:
 	// http://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/Humidity/Sensirion_Humidity_SHT1x_Datasheet_V5.pdf
@@ -86,7 +154,7 @@ double sht11_relative_humidity(unsigned raw)
 	return c1 + c2 * raw + c3 * raw * raw;
 }
 
-double sht11_relative_humidity_compensated(unsigned raw, double temperature)
+static double sht11_relative_humidity_compensated(unsigned raw, double temperature)
 {
 	// 12-bit
 	static const double t1 = 0.01;
@@ -98,11 +166,16 @@ double sht11_relative_humidity_compensated(unsigned raw, double temperature)
 
 	double humidity = sht11_relative_humidity(raw);
 
-	return (temperature - 25) * (t1 + t2 * raw) + humidity;
+	humidity = (temperature - 25) * (t1 + t2 * raw) + humidity;
+
+	//if (humidity > 99)
+	//	humidity = 100;
+
+	return humidity;
 }
 
 /** Output temperature in degrees Celcius */
-double sht11_temperature(unsigned raw)
+static double sht11_temperature(unsigned raw)
 {
 	//static const double d1 = -40.1; // 5V
 	//static const double d1 = -39.8; // 4V
@@ -116,35 +189,46 @@ double sht11_temperature(unsigned raw)
 	return d1 + d2 * raw;
 }
 
-
-/** The structure of the message we are sending */
-typedef struct collect_msg
-{
-	double temperature;
-	double humidity;
-
-} collect_msg_t;
-
 /** The function that will be executed when a message is received */
 static void recv(rimeaddr_t const * originator, uint8_t seqno, uint8_t hops)
 {
-	collect_msg_t const * msg;
+	base_msg_t const * bmsg = (base_msg_t const *)packetbuf_dataptr();
 
-	msg = (collect_msg_t const *)packetbuf_dataptr();
+	switch (bmsg->type)
+	{
+		case collect_message_type:
+		{
+			collect_msg_t const * msg = (collect_msg_t const *)bmsg;
 
-	printf("Network Data: Addr:%d.%d Seqno:%u Hops:%u Temp:%d Hudmid:%d%%\n",
-		originator->u8[0], originator->u8[1],
-		seqno, hops,
-		(int)msg->temperature, (int)msg->humidity
-	);
+			printf("Network Data: Addr:%d.%d Seqno:%u Hops:%u Temp:%d Hudmid:%d%%\n",
+				originator->u8[0], originator->u8[1],
+				seqno, hops,
+				(int)msg->temperature, (int)msg->humidity
+			);
+		} break;
+
+		case error_message_type:
+		{
+			error_msg_t const * msg = (error_msg_t const *)bmsg;
+
+			printf("Error occured on %d.%d: %s",
+				originator->u8[0], originator->u8[1],
+				msg->contents
+			);
+
+		} break;
+
+		default:
+		{
+			printf("Unknown message type %u (%s)\n", bmsg->type, message_type_to_string(bmsg->type));
+		} break;
+	}
+
+	
 }
 
 /** List of all functions to execute when a message is received */
 static const struct collect_callbacks callbacks = { recv };
-
-static struct collect_conn tc;
-
-static const int REXMITS = 4;
 
  
 PROCESS(data_collector_process, "Data Collector");
@@ -155,6 +239,7 @@ PROCESS_THREAD(data_collector_process, ev, data)
 {
 	static struct etimer et;
 	static unsigned raw_humidity, raw_temperature;
+	static double humidity, temperature;
  
 	PROCESS_EXITHANDLER(goto exit;)
 	PROCESS_BEGIN();
@@ -168,17 +253,11 @@ PROCESS_THREAD(data_collector_process, ev, data)
 		collect_set_sink(&tc, 1);
 	}
 
-	etimer_set(&et, CLOCK_SECOND);
+	etimer_set(&et, 15 * CLOCK_SECOND);
  
-	while (1)
+	while (true)
 	{
-		collect_msg_t * msg;
-
 		PROCESS_YIELD();
-
-		packetbuf_clear();
-		packetbuf_set_datalen(sizeof(collect_msg_t));
-		msg = (collect_msg_t *)packetbuf_dataptr();
 
 		SENSORS_ACTIVATE(sht11_sensor);
 
@@ -189,15 +268,22 @@ PROCESS_THREAD(data_collector_process, ev, data)
 
 		/* http://arduino.cc/forum/index.php?topic=37752.5;wap2 */
 
-		msg->temperature = sht11_temperature(raw_temperature);
-		msg->humidity = sht11_relative_humidity_compensated(raw_humidity, msg->temperature);
+		temperature = sht11_temperature(raw_temperature);
+		humidity = sht11_relative_humidity_compensated(raw_humidity, temperature);
+
+		check_predicate(&temperature_validator, &temperature_message, &temperature);
+		check_predicate(&humidity_validator, &humidity_message, &humidity);
+
+
+		packetbuf_clear();
+		packetbuf_set_datalen(sizeof(collect_msg_t));
+		collect_msg_t * msg = (collect_msg_t *)packetbuf_dataptr();
+
+		msg->base.type = collect_message_type;
+		msg->temperature = temperature;
+		msg->humidity = humidity;
 		
-		collect_send(&tc, REXMITS);
-
-
-		check_predicate(&temperature_validator, &temperature_message, &(msg->temperature));
-		check_predicate(&humidity_validator, &humidity_message, &(msg->humidity));
-
+		collect_send(&tc, NORMAL_REXMITS);
 
 		etimer_reset(&et);
 	}

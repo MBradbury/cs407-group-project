d37ea30bb67bcd241c8d2f4f30880f9c03005197
Amit Shah <amitshah@oneuk.com>
2012-11-06 16:13:30 +0000

    Fixed Node.c to communicate properly.
     Messages send from nodes are now recieved properly by the sink node.
    The sink does not print it's own message when receiving.
    
    Cleaned up the code a little

--- Algorithms/hsend/Node.c
+++ Algorithms/hsend/Node.c
@@ -11,6 +11,7 @@
 
 #include "net/netstack.h"
 #include "net/rime.h"
+
 #include "net/rime/collect.h"
 #include "net/rime/collect-neighbor.h"
 #include "net/rime/timesynch.h"
@@ -21,18 +22,11 @@
 /** The function that will be executed when a message is received */
 static void recv(rimeaddr_t const * originator, uint8_t seqno, uint8_t hops)
 {
-	//printf("Message received\n");
-
-	//Print the message received
-	printf("Message: %s\n",(char *)packetbuf_dataptr() );
-
-	//TODO, don't broadcast when receiving own message
+	//if message was from another node, print it out
 	if (rimeaddr_cmp(&rimeaddr_node_addr,originator) == 0)
 	{
-		printf("Sending received message\n");
-		sendRIMEMessage("Message received");
+		printf("Message received from %d: %s \n",address->u8[0],(char *)packetbuf_dataptr() );
 	}
-
 }	
 
 /** List of all functions to execute when a message is received */
@@ -47,24 +41,42 @@ AUTOSTART_PROCESSES(&messageSenderProcess);
 /* Method will broadcast a message using RIME */
 void sendRIMEMessage(char * message)
 {
-		//open the connection, along with the callback function
-		//TODO: some nodes can't be set to 1, as they will be the sink, Check with ID if ==1 become sink
-		collect_set_sink(&tc, 1);
-	
 		packetbuf_clear(); //clear the buffer
+		packetbuf_set_datalen(sizeof(message));
+
 		packetbuf_copyfrom(message, strlen(message)); //copy the message to the buffer
 		collect_send(&tc, REXMITS); //send the buffer over tc
-		//collect_close(&tc); //close the connection
 }
 
 
 PROCESS_THREAD(messageSenderProcess, ev, data)
 {
 	PROCESS_BEGIN();
-	collect_open(&tc, 128, COLLECT_ROUTER, &callbacks);
 
-	printf("Sending initial message\n"); //print to the log
-	sendRIMEMessage("Hello World again!");
+	//open the connection, along with the callback function
+	collect_open(&tc, 128, COLLECT_ROUTER, &callbacks);
+	
+	//if the address of the node is 1, set to the sink
+	if (rimeaddr_node_addr.u8[0] == 1 &&
+	rimeaddr_node_addr.u8[1] == 0)
+	{
+		collect_set_sink(&tc, 1);
+	}
+	else 
+	{
+		collect_set_sink(&tc,0);
+	}
+
+	//if the sink node, seend Hello World!
+	if (rimeaddr_node_addr.u8[0] == 1)
+	{
+		sendRIMEMessage("Hello World!");
+
+	}
+	else 
+	{
+		sendRIMEMessage("Hello World again!");
+	}
 
 	PROCESS_END();
 }

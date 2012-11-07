f21477d46a9a6a41a6b94f61bd09bab4d86682af
Amit Shah <amitshah@oneuk.com>
2012-11-07 15:28:54 +0000

    Added updated files

--- Algorithms/hsend/Node.c
+++ Algorithms/hsend/Node.c
@@ -11,14 +11,13 @@
 static struct mesh_conn mesh;
 static rimeaddr_t dest;
 
-const static struct mesh_callbacks callbacks = {recv, sent, timedout};
 
 /** The function that will be executed when a message is received */
 static void 
 recv(struct mesh_conn *c, const rimeaddr_t *from, uint8_t hops)
 
 {
-	printf("Message received from: %d.%d message: %s\n",from->u8[0],from->u8[1],(char *)packetbuf_dataptr());
+	printf("Message received from: %s message: %s\n",addr2str(from),(char *)packetbuf_dataptr());
 }	
 
 static void
@@ -32,10 +31,18 @@ timedout(struct mesh_conn *c)
   printf("packet timedout\n");
 }
 
+const static struct mesh_callbacks callbacks = {recv, sent, timedout};
+
 static void 
 sendToBaseStation(char * message)
 {
+	
 
+	packetbuf_clear();
+	packetbuf_set_datalen(strlen(message));
+	packetbuf_copyfrom(message, strlen(message));
+
+	mesh_send(&mesh, &dest);
 }
 
 PROCESS(messageSenderProcess, "Message Sender");
@@ -43,7 +50,39 @@ AUTOSTART_PROCESSES(&messageSenderProcess);
 
 PROCESS_THREAD(messageSenderProcess, ev, data)
 {
+	static struct etimer et;
 
 
 
+	PROCESS_EXITHANDLER(goto exit;)
+	PROCESS_BEGIN();
+
+	mesh_open(&mesh, 147, &callbacks);
+
+	//set the base station
+	memset(&dest, 0, sizeof(rimeaddr_t));
+	dest.u8[sizeof(rimeaddr_t) - 2] = 1;
+
+	etimer_set(&et, 10 * CLOCK_SECOND); //10 second timer
+
+	while(1)
+	{
+
+		char *message = "Hello World!!";
+		char output[21];
+		strcat(output,message);
+		strcat(output,addr2str(&rimeaddr_node_addr));
+
+		sendToBaseStation(output);
+
+
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+		etimer_set(&et, 10 * CLOCK_SECOND); //10 second timer
+
+	}
+
+	exit:
+		printf("Exiting...\n");
+		mesh_close(&mesh);
+		PROCESS_END();
 }	

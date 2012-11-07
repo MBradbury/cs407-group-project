3962485462d3a456fcc66b0bea38eff1a64c30b5
Joe Yarnall <joe.yarnall@gmail.com>
2012-11-07 16:24:40 +0100

    commit

--- Algorithms/hsend/MeshNode.c
+++ Algorithms/hsend/MeshNode.c
@@ -15,47 +15,57 @@
 
 static struct mesh_conn mesh;
 static rimeaddr_t dest;
+static struct etimer et;
 
 /** The function that will be executed when a message is received */
 static void recv(struct mesh_conn *c, const rimeaddr_t *from, uint8_t hops)
-
 {
 	printf("Message received from: %d.%d message: %s\n",from->u8[0],from->u8[1],(char *)packetbuf_dataptr());
-}	
+}
 
-static void
-sent(struct mesh_conn *c)
+/** The function that will be executed when a message is sent */
+static void sent(struct mesh_conn *c)
 {
   printf("packet sent\n");
 }
-static void
-timedout(struct mesh_conn *c)
+
+/** The function that will be executed when a message timesout */
+static void timedout(struct mesh_conn *c)
 {
   printf("packet timedout\n");
 }
 
 const static struct mesh_callbacks callbacks = {recv, sent, timedout};
 
+PROCESS(meshSetupProcess, "Mesh Setup");
 PROCESS(messageSenderProcess, "Message Sender");
-AUTOSTART_PROCESSES(&messageSenderProcess);
+AUTOSTART_PROCESSES(&meshSetupProcess, &messageSenderProcess);
 
-PROCESS_THREAD(messageSenderProcess, ev, data)
+PROCESS_THREAD(meshSetupProcess, ev, data)
 {
-	static struct etimer et;
-
-	PROCESS_EXITHANDLER(goto exit;)
 	PROCESS_BEGIN();
 
 	mesh_open(&mesh, 147, &callbacks);
 
-	etimer_set(&et, 10 * CLOCK_SECOND);
-
 	memset(&dest, 0, sizeof(rimeaddr_t));
 	dest.u8[sizeof(rimeaddr_t) - 2] = 1;
+
+	etimer_set(&et, 10 * CLOCK_SECOND);
+
+	printf("Exiting Mesh Setup Process...\n");
+	PROCESS_END();
+}
+
+PROCESS_THREAD(messageSenderProcess, ev, data)
+{
+	PROCESS_EXITHANDLER(goto exit;)
+	PROCESS_BEGIN();
 
 	while(1) 
 	{
 	    char * msg = "This is a message";
+
+	    etimer_set(&et, 10 * CLOCK_SECOND);
 
 		packetbuf_clear();
 		packetbuf_set_datalen(strlen(msg));
@@ -73,7 +83,8 @@ PROCESS_THREAD(messageSenderProcess, ev, data)
 	}
 
  exit:
-	printf("Exiting...\n");
+	printf("Exiting Message Sending Process...\n");
 	mesh_close(&mesh);
 	PROCESS_END();
 }
+

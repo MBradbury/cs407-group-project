1e180aac110230748b82bc93cea2d75e79f9fb0e
Joe Yarnall <joe.yarnall@gmail.com>
2012-11-08 14:36:29 +0100

    Consolidated Node and Base files

--- Algorithms/hsend/Node.c
+++ Algorithms/hsend/Node.c
@@ -12,24 +12,56 @@ static struct mesh_conn mesh;
 static rimeaddr_t dest;
 static int messageSent = 0;
 
+static bool isBase()
+{
+	rimeaddr_t base;
+	memset(&base, 0, sizeof(rimeaddr_t));
+	base.u8[sizeof(rimeaddr_t) - 2] = 1;
+
+	return rimeaddr_cmp(&rimeaddr_node_addr, &base) == 1;
+}
+
 /** The function that will be executed when a message is received */
 static void 
 recv(struct mesh_conn *c, const rimeaddr_t *from, uint8_t hops)
-
 {
-	printf("Message received from: %s message: %s\n",addr2str(from),(char *)packetbuf_dataptr());
+	if(isBase())
+	{
+		printf("Message received from: %s message: %s\n",addr2str(from),(char *)packetbuf_dataptr());
+	}
+	else
+	{
+
+	}
 }	
 
 static void
 sent(struct mesh_conn *c)
 {
-	messageSent = 1;
-  printf("packet sent\n");
+	if(isBase())
+	{
+
+	}
+	else
+	{
+		messageSent = 1;
+  		printf("packet sent\n");
+	}
 }
+
 static void
 timedout(struct mesh_conn *c)
 {
-  printf("packet timedout\n");
+  	printf("packet timedout\n");
+
+	if(isBase())
+	{
+
+	}
+	else
+	{
+
+	}
 }
 
 const static struct mesh_callbacks callbacks = {recv, sent, timedout};
@@ -44,16 +76,16 @@ sendToBaseStation(char * message)
 	mesh_send(&mesh, &dest); //send the message
 }
 
-PROCESS(messageSenderProcess, "Message Sender");
-AUTOSTART_PROCESSES(&messageSenderProcess);
+PROCESS(networkInit, "Network Init");
+PROCESS(nodeProcess, "Node Process");
+PROCESS(baseStation, "Sink Process");
 
-PROCESS_THREAD(messageSenderProcess, ev, data)
+AUTOSTART_PROCESSES(&networkInit);
+
+PROCESS_THREAD(networkInit, ev, data)
 {
 	static struct etimer et;
 
-
-
-	PROCESS_EXITHANDLER(goto exit;)
 	PROCESS_BEGIN();
 
 	mesh_open(&mesh, 147, &callbacks);
@@ -62,20 +94,60 @@ PROCESS_THREAD(messageSenderProcess, ev, data)
 	memset(&dest, 0, sizeof(rimeaddr_t));
 	dest.u8[sizeof(rimeaddr_t) - 2] = 1;
 
-	etimer_set(&et, 10 * CLOCK_SECOND); //10 second timer
+	//5 second timer
+	etimer_set(&et, 5 * CLOCK_SECOND);
+	PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+	if (isBase())
+	{
+		process_start(&baseStation, NULL);
+	}
+	else
+	{
+		process_start(&nodeProcess, NULL);
+	}
+
+	PROCESS_END();
+}
+
+PROCESS_THREAD(nodeProcess, ev, data)
+{
+	static struct etimer et;
+
+	PROCESS_EXITHANDLER(goto exit;)
+	PROCESS_BEGIN();
 
 	while(1)
 	{
-		if (messageSent == 0) {
-		char *message = "Hello World!!";
-		sendToBaseStation(message);
-		}
-		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 		etimer_set(&et, 10 * CLOCK_SECOND); //10 second timer
+
+		if (messageSent == 0) 
+		{
+			char *message = "Hello World!!";
+			sendToBaseStation(message);
+		}
+
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 	}
 
 	exit:
-		printf("Exiting...\n");
+		printf("Exiting Node Process...\n");
+		mesh_close(&mesh);
+		PROCESS_END();
+}
+
+PROCESS_THREAD(baseStation, ev, data)
+{
+	PROCESS_EXITHANDLER(goto exit;)
+	PROCESS_BEGIN();
+
+	while(1)
+	{
+
+	}
+
+	exit:
+		printf("Exiting Base Process...\n");
 		mesh_close(&mesh);
 		PROCESS_END();
 }	

32418a5ac4e284c34b8e5438b8795993952e47ae
Amit Shah <amitshah@oneuk.com>
2012-11-18 14:39:43 +0000

    Messages now forwarded on to root node, not all messages get through however

--- Algorithms/hsend/Node.c
+++ Algorithms/hsend/Node.c
@@ -32,11 +32,11 @@ stbroadcast_recv(struct stbroadcast_conn *c)
     packetbuf_copyto(tmpBuffer);
     predicate_check_msg_t *msg = (predicate_check_msg_t *)tmpBuffer;
 
-	printf("I just recieved a Stubborn Broadcast Message! Originator: %s Message: %s Hop: %d Message ID: %d\n", 
-		addr2str(&msg->originator), 
-		msg->predicate_to_check,
-		msg->hop_limit,
-		msg->message_id);
+//	printf("I just recieved a Stubborn Broadcast Message! Originator: %s Message: %s Hop: %d Message ID: %d\n", 
+//		addr2str(&msg->originator), 
+//		msg->predicate_to_check,
+//		msg->hop_limit,
+//		msg->message_id);
 
 	// Check message has not been recieved before
 	bool deliver_msg = false;
@@ -117,18 +117,62 @@ static void
 runicast_recv(struct runicast_conn *c, const rimeaddr_t *from, uint8_t seqno)
 {
 	printf("runicast received from %s\n", addr2str(from));
+
+	//when recieve message, forward the message on to the originator
+	//if the final originator, do something with the value
+
+	//Copy Packet Buffer To Memory
+    char tmpBuffer[PACKETBUF_SIZE];
+    packetbuf_copyto(tmpBuffer);
+    predicate_return_msg_t *msg = (predicate_return_msg_t *)tmpBuffer;
+
+
+    struct list_elem_struct * list_iterator = NULL;
+	for ( list_iterator = (struct list_elem_struct *)list_head(message_list);
+		  list_iterator != NULL;
+		  list_iterator = (struct list_elem_struct *)list_item_next(&list_iterator)
+		)
+	{
+		if (list_iterator->message_id == msg->message_id)
+		{
+			if (rimeaddr_cmp(&rimeaddr_node_addr,&list_iterator->originator)) //if this node was the one who sent the message
+			{
+				printf("End of forwarding Value: %s received from: %s\n",msg->evaluated_predicate, addr2str(&msg->sender));
+			}
+			else 
+			{
+				rimeaddr_t * dest = (rimeaddr_t *) malloc(sizeof(rimeaddr_t));
+				rimeaddr_copy(&dest,&list_iterator->originator);
+				printf("Trying to forward evaluated predicate to: %s\n", addr2str(&dest));
+	
+					send_evaluated_predicate(&msg->sender, 
+						dest,//&list_iterator->originator,  //TODO not being passed properly!!!!
+						list_iterator->message_id, 
+						msg->evaluated_predicate
+					);
+				free(dest);
+			}
+			break;
+		}
+	}
+
+	if (list_iterator == NULL)
+	{
+		printf("DEBUG: ERROR - LIST IS NULL, THIS IS VERY VERY BAD\n");
+	}
+
 }
 
 static void
 runicast_sent(struct runicast_conn *c, const rimeaddr_t *to, uint8_t retransmissions)
 {
-	printf("runicast sent\n");
+	//printf("runicast sent\n");
 }
 
 static void
 runicast_timedout(struct runicast_conn *c, const rimeaddr_t *to, uint8_t retransmissions)
 {
-	printf("Runicast timed out to:%s retransmissions:%d\n", addr2str(&to), retransmissions);
+	printf("Runicast timed out to:%s retransmissions:%d\n", addr2str(to), retransmissions);
 }
 
 
@@ -144,12 +188,12 @@ delayed_send_evaluated_predicate(uint8_t message_id)
 	{
 		if (list_iterator->message_id == message_id)
 		{
-			printf("Trying to send evaluated predicate to: %s\n", addr2str(&list_iterator->originator));
 			rimeaddr_t * dest = (rimeaddr_t *) malloc(sizeof(rimeaddr_t));
 			rimeaddr_copy(&dest,&list_iterator->originator);
+			printf("Trying to send evaluated predicate to: %s\n", addr2str(&dest));
 
 			send_evaluated_predicate(&rimeaddr_node_addr, 
-				dest,//&list_iterator->originator,  //TODO not being passed properly!!!!
+				dest,//&list_iterator->originator, 
 				list_iterator->message_id, 
 				evaluate_predicate(&list_iterator->predicate_to_check)
 			);
@@ -280,7 +324,8 @@ PROCESS_THREAD(mainProcess, ev, data)
 				struct list_elem_struct * delivered_msg =  (struct list_elem_struct *)malloc(sizeof(struct list_elem_struct));
 				delivered_msg->message_id = get_message_id();
 				delivered_msg->hops = 2;
-
+				//set the originator to self
+				rimeaddr_copy(&delivered_msg->originator,&rimeaddr_node_addr); 
 				list_push(message_list, delivered_msg);
 
 				send_n_hop_predicate_check(&rimeaddr_node_addr, delivered_msg->message_id, "Hello World!!!", delivered_msg->hops);

84a071e25ad44ce7810453f4e0f74718661abe87
Daniel Robertson <csujbm@mscs-16.(none)>
2012-12-04 10:19:20 +0000

    Actually added stuff this time

---
+++ Algorithms/Common/logging.h
@@ -0,0 +1,14 @@
+#ifndef CS407_LOGGING_H
+#define CS407_LOGGING_H
+
+#include "net/netstack.h"
+#include "net/rime.h"
+#include "net/rime/stbroadcast.h"
+#include "net/rime/runicast.h"
+#include "net/rime/mesh.h"
+
+log_msg_t * return_log(void);
+void log_message(clock_time_t time, char * msg_type, char * send_type, rimeaddr_t from, rimeaddr_t to);
+
+#endif /*CS407_LOGGING_H*/
+

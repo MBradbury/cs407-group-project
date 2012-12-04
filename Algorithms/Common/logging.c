9dfe3b83d8cb67b3010da89ba216c908a8f25c9c
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2012-12-04 10:58:17 +0000

    Logging

--- Algorithms/Common/logging.c
+++ Algorithms/Common/logging.c
@@ -1,55 +1,45 @@
-#include "contiki.h"
-
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <limits.h>
-
 #include "net/netstack.h"
-#include "net/rime.h"
-#include "net/rime/stbroadcast.h"
-#include "net/rime/runicast.h"
-#include "net/rime/mesh.h"
-#include "contiki-net.h"
 #include "sys/clock.h"
 
 #include "logging.h"
 #include "debug-helper.h"
 
-typedef struct
+#define LOG_SIZE 10
+
+static unsigned int number = 0;
+
+static log_info_t log[LOG_SIZE];
+
+log_info_t * log_start(void)
 {
-	clock_time_t time;
-	rimeaddr_t source;
-	rimeaddr_t dest;
 	
-	char * msg_type;
-	char * send_type;
-
-} log_msg_t;
-
-static int number = 0;
-static log_msg_t log[10];
-
-log_msg_t * return_log()
-{
-	return log;
 }
 
-void log_message(clock_time_t time, char * msg_type, char * send_type, rimeaddr_t from, rimeaddr_t to)
+log_info_t * log_next(log_info_t * log)
+{
+	
+}
+
+void log_message(clock_time_t time, char * msg_type, char * send_type, rimeaddr_t const * from, rimeaddr_t const * to)
 {
 	char from_str[RIMEADDR_STRING_LENGTH];
 	char to_str[RIMEADDR_STRING_LENGTH];
+
 	//printf("Logging %s %s message (time %u, pos %d): From %s To %s\n", msg_type, send_type, time, number, addr2str_r(&from, from_str, RIMEADDR_STRING_LENGTH), addr2str_r(&to, to_str, RIMEADDR_STRING_LENGTH));
 	printf("%s\n", addr2str_r(&from, from_str, RIMEADDR_STRING_LENGTH));
 	printf("%s\n", addr2str_r(&to, to_str, RIMEADDR_STRING_LENGTH));
 	printf("%d\n", number);
+
 	log_msg_t msg;
+
 	msg.time = time;
 	msg.source = from;
 	msg.dest = to;
 	msg.msg_type = msg_type;
 	msg.send_type = send_type;
+
 	log[number] = msg;
-	number = (number + 1) % 10;
+
+	number = (number + 1) % LOG_SIZE;
 }
 
--- Algorithms/Common/logging.h
+++ Algorithms/Common/logging.h
@@ -1,14 +1,29 @@
 #ifndef CS407_LOGGING_H
 #define CS407_LOGGING_H
 
-#include "net/netstack.h"
-#include "net/rime.h"
-#include "net/rime/stbroadcast.h"
-#include "net/rime/runicast.h"
-#include "net/rime/mesh.h"
+typedef struct
+{
+	clock_time_t time;
 
-log_msg_t * return_log(void);
-void log_message(clock_time_t time, char * msg_type, char * send_type, rimeaddr_t from, rimeaddr_t to);
+	rimeaddr_t source;
+	rimeaddr_t dest;
+	
+	char const * msg_type;
+	char const * send_type;
+
+} log_info_t;
+
+// Returns the oldest message in the log
+log_info_t * log_start(void);
+
+// Gets the next message in the log.
+// Returns NULL if there are no messages left to consume.
+log_info_t * log_next(log_info_t * log);
+
+void log_message(
+	clock_time_t time,
+	char const * msg_type, char const * send_type,
+	rimeaddr_t const * from, rimeaddr_t const * to);
 
 #endif /*CS407_LOGGING_H*/
 

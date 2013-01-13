f1802863f4fe7ec392efcd2ff955b32725853c29
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2013-01-13 20:10:12 +0000

    Fix a float parsing issue with hoppy

--- PredicateLanguage/Hoppy/run.sh
+++ PredicateLanguage/Hoppy/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-make; echo -e "[all] using Neighbours(2) as twohopn in F(this) == 2 + 1 & 5 > 3" | java -cp . Hoppy
+make; echo -e "[all] using Neighbours(2) as twohopn in F(this) == 2.0 + 1 & 5 > 3" | java -cp . Hoppy
 

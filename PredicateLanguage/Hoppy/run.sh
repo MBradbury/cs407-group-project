c750587b52112666a143a6fe54bca5217a11dbb1
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2013-01-13 20:08:48 +0000

    Lots of work on hoppy to support infix notation

--- PredicateLanguage/Hoppy/run.sh
+++ PredicateLanguage/Hoppy/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-make; echo -e "[all] using Neighbours(2) as twohopn in & < ** x(y) 2 3 > y 5" | java -cp . Hoppy
+make; echo -e "[all] using Neighbours(2) as twohopn in F(this) == 2 + 1 & 5 > 3" | java -cp . Hoppy
 

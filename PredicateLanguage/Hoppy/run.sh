435318943d7301883470ea69810b0f9730194d75
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2013-01-14 13:04:22 +0000

    Add Function Definitions

--- PredicateLanguage/Hoppy/run.sh
+++ PredicateLanguage/Hoppy/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-make; echo -e "[01,00] using Neighbours(2) as twohopn in @(x : twohopn ~ fn(x) == abs(6 - 9 * 7) + len(twohopn) + mean(twohopn, temperature))" | java -cp . Hoppy
+make; echo -e "[01,00] function 0 as temperature returning float in using Neighbours(2) as twohopn in ! @(x : twohopn ~ fn(x) == abs(6 - 9 * 7) + len(twohopn) + mean(twohopn, temperature))" | java -cp . Hoppy
 

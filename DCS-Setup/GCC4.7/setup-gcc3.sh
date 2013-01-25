611e4b0a7c06348e59871ba5f1e7e075589c1d13
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2013-01-25 11:40:35 +0000

    GCC4.7 setup

---
+++ DCS-Setup/GCC4.7/setup-gcc3.sh
@@ -0,0 +1,37 @@
+#http://wiki.contiki-os.org/doku.php?id=msp430x
+#http://comments.gmane.org/gmane.os.contiki.devel/13869
+INSTALL_PREFIX="/var/tmp/mspgcc"
+cd /tmp/mspgcc4.7
+
+# 3) Creating new directories
+mkdir binutils-2.22-msp430
+mkdir gcc-4.7.0-msp430
+
+# 4) installing binutils in INSTALL_PREFIX
+cd binutils-2.22-msp430/
+../binutils-2.22/configure --target=msp430 --program-prefix="msp430-" --prefix=$INSTALL_PREFIX
+make -j 5
+make install
+
+# 6) compiling gcc-4.7.0 in INSTALL_PREFIX
+cd ../gcc-4.7.0-msp430
+../gcc-4.7.0/configure --target=msp430 --enable-languages=c --program-prefix="msp430-" --prefix=$INSTALL_PREFIX
+make -j 5
+make install
+
+# 7) compiping msp430mcu in INSTALL_PREFIX
+cd ../msp430mcu-20120716
+export MSP430MCU_ROOT=`pwd`
+./scripts/install.sh ${INSTALL_PREFIX}/
+
+# 8) compiling the msp430 lib in INSTALL_PREFIX
+cd ../msp430-libc-20120716
+cd src
+export PATH=${INSTALL_PREFIX}/bin:$PATH
+make -j 5
+make PREFIX=$INSTALL_PREFIX install
+
+# cleanup
+# no need since every thing created in tmp
+echo Reminder: remove tmp
+

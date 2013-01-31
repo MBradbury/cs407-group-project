9f9f63d423157d01eb5acd0534e362c79484cee0
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2013-01-31 17:20:37 +0000

    Modify HSend to support evaluating multiple predicates

--- Algorithms/HSend/hsend.c
+++ Algorithms/HSend/hsend.c
@@ -191,9 +191,39 @@ static rimeaddr_t baseStationAddr;
 static const clock_time_t trickle_interval = 2 * CLOCK_SECOND;
 
 
-PROCESS(hsendProcess, "HSEND Process");
 
-static ubyte const program_bytecode[] = {0x30,0x01,0x01,0x01,0x00,0x01,0x00,0x00,0x06,0x01,0x0a,0xff,0x1c,0x13,0x31,0x30,0x02,0x01,0x00,0x00,0x01,0x00,0x00,0x06,0x02,0x0a,0xff,0x1c,0x13,0x2c,0x37,0x01,0xff,0x00,0x37,0x02,0xff,0x00,0x1b,0x2d,0x35,0x02,0x12,0x19,0x2c,0x35,0x01,0x12,0x0a,0x00};
+typedef struct
+{
+	uint8_t id; // Keep id as the first variable in the struct then
+	uint8_t variables_details_length;
+	uint8_t bytecode_length;
+
+	var_elem_t * variables_details;
+	ubyte const * bytecode;
+
+} predicate_detail_entry_t;
+
+static void predicate_detail_entry_cleanup(void * item)
+{
+	predicate_detail_entry_t * entry = (predicate_detail_entry_t *)item;
+
+	free(entry->variables_details);
+	free(entry->bytecode);
+	free(entry);
+}
+
+static bool predicate_id_equal(void const * left, void const * right)
+{
+	if (left == NULL || right == NULL)
+		return false;
+
+	predicate_detail_entry_t const * l = (predicate_detail_entry_t const *)left;
+	predicate_detail_entry_t const * r = (predicate_detail_entry_t const *)right;
+
+	return l->id == r->id;
+}
+
+static map_t predicate_map;
 
 
 // Rime address of target node (or rimeaddr_null for everyone)
@@ -211,23 +241,114 @@ static void trickle_rcv(struct trickle_conn * c)
 	if (rimeaddr_cmp(target, &rimeaddr_null) ||		// Send to all
 		rimeaddr_cmp(target, &rimeaddr_node_addr)) 	// We are the target
 	{
-		eval_pred_req_t * msgcopy = (eval_pred_req_t *)malloc(packetbuf_datalen());
-		memcpy(msgcopy, msg, packetbuf_datalen());
-
 		leds_on(LEDS_RED);
 		leds_off(LEDS_GREEN);
 
-		// Start HSEND
-		printf("Got message, starting evaluation!\n");
-		process_start(&hsendProcess, msgcopy);
+		// Add or update entry
+		predicate_detail_entry_t * stored = (predicate_detail_entry_t *)map_get(&predicate_map, &msg->predicate_id);
+
+		if (stored)
+		{
+			printf("Updating predicate with id %d.\n", msg->predicate_id);
+
+			// Re-allocate data structures if needed
+
+			if (msg->bytecode_length != stored->bytecode_length)
+			{
+				free(stored->bytecode);
+				stored->bytecode = malloc(sizeof(ubyte) * msg->bytecode_length);
+			}
+
+			if (msg->num_of_bytecode_var != stored->variables_details_length)
+			{
+				free(stored->variables_details);
+				stored->variables_details = malloc(sizeof(var_elem_t) * msg->num_of_bytecode_var);
+			}
+		}
+		else
+		{
+			printf("Creating predicate with id %d.\n", msg->predicate_id);
+
+			// Allocate memory for the data
+			stored = malloc(sizeof(predicate_detail_entry_t));
+
+			stored->bytecode = malloc(sizeof(ubyte) * msg->bytecode_length);
+			stored->variables_details = malloc(sizeof(var_elem_t) * msg->num_of_bytecode_var);
+
+			// Put data in the map
+			map_put(&predicate_map, stored);
+		}
+
+		// Pointer for bytecode variables
+		var_elem_t const * variables = (var_elem_t const *)(msg + 1);
+
+		// Create a pointer to the bytecode instructions stored in the message.
+		ubyte const * bytecode_instructions = (ubyte const *)(variables + msg->num_of_bytecode_var);
+
+		// Update data
+		stored->bytecode_length = msg->bytecode_length;
+		stored->variables_details_length = msg->num_of_bytecode_var;
+
+		memcpy(stored->bytecode, bytecode_instructions, sizeof(ubyte) * stored->bytecode_length);
+		memcpy(stored->variables_details, variables, sizeof(var_elem_t) * stored->variables_details_length);
 	}
 }
 
 static const struct trickle_callbacks callbacks = { &trickle_rcv };
 
 PROCESS(mainProcess, "MAIN Process");
+PROCESS(hsendProcess, "HSEND Process");
 
-AUTOSTART_PROCESSES(&mainProcess);
+AUTOSTART_PROCESSES(&mainProcess, &hsendProcess);
+
+
+static void send_example_predicate(rimeaddr_t const * destination)
+{
+	static ubyte const program_bytecode[] = {0x30,0x01,0x01,0x01,0x00,0x01,0x00,0x00,0x06,0x01,0x0a,0xff,0x1c,0x13,0x31,0x30,0x02,0x01,0x00,0x00,0x01,0x00,0x00,0x06,0x02,0x0a,0xff,0x1c,0x13,0x2c,0x37,0x01,0xff,0x00,0x37,0x02,0xff,0x00,0x1b,0x2d,0x35,0x02,0x12,0x19,0x2c,0x35,0x01,0x12,0x0a,0x00};
+
+	// Send the request message
+
+	uint8_t bytecode_length = sizeof(program_bytecode)/sizeof(program_bytecode[0]);
+	uint8_t var_details = 2;
+	
+	unsigned int packet_size = sizeof(eval_pred_req_t) + bytecode_length + (sizeof(var_elem_t) * var_details);
+
+	packetbuf_clear();
+	packetbuf_set_datalen(packet_size);
+	debug_packet_size(packet_size);
+	eval_pred_req_t * msg = (eval_pred_req_t *)packetbuf_dataptr();
+	memset(msg, 0, packet_size);
+
+	// Set eventual destination in header
+	packetbuf_set_addr(PACKETBUF_ADDR_ERECEIVER, destination);
+
+	msg->predicate_id = 0;
+	msg->bytecode_length = bytecode_length;
+	msg->num_of_bytecode_var = var_details;
+
+	var_elem_t * msg_vars = (var_elem_t *)(msg + 1);
+
+	msg_vars[0].hops = 2;
+	msg_vars[0].var_id = 255;
+	msg_vars[1].hops = 1;
+	msg_vars[1].var_id = 254;
+
+	ubyte * msg_bytecode = (ubyte *)(msg_vars + var_details);
+
+	// Debug check to make sure that we have done sane things!
+	if ((void *)(msg_bytecode + bytecode_length) - (void *)msg != packet_size)
+	{
+		printf("Failed to copy data correctly got=%d expected=%d!\n",
+			(void *)(msg_bytecode + bytecode_length) - (void *)msg,
+			packet_size);
+	}
+
+	memcpy(msg_bytecode, program_bytecode, bytecode_length);
+
+	//printf("Sent packet length %d\n", packet_size);
+
+	trickle_send(&tc);
+}
 
 PROCESS_THREAD(mainProcess, ev, data)
 {
@@ -242,6 +363,8 @@ PROCESS_THREAD(mainProcess, ev, data)
 #endif
 
 	array_list_init(&hops_data, &free_hops_data);
+
+	map_init(&predicate_map, &predicate_id_equal, &predicate_detail_entry_cleanup);
 
 	// Set the address of the base station
 	baseStationAddr.u8[0] = 1;
@@ -273,48 +396,7 @@ PROCESS_THREAD(mainProcess, ev, data)
 	{
 		printf("Is the base station!\n");
 
-		// Send the request message
-		uint8_t bytecode_length = sizeof(program_bytecode)/sizeof(program_bytecode[0]);
-		uint8_t var_details = 2;
-		
-		unsigned int packet_size = sizeof(eval_pred_req_t) + bytecode_length + (sizeof(var_elem_t) * var_details);
-
-		packetbuf_clear();
-		packetbuf_set_datalen(packet_size);
-		debug_packet_size(packet_size);
-		eval_pred_req_t * msg = (eval_pred_req_t *)packetbuf_dataptr();
-		memset(msg, 0, packet_size);
-
-		// Set eventual destination in header
-		packetbuf_set_addr(PACKETBUF_ADDR_ERECEIVER, &destination);
-
-		msg->predicate_id = 0;
-		msg->bytecode_length = bytecode_length;
-		msg->num_of_bytecode_var = var_details;
-
-		var_elem_t * msg_vars = (var_elem_t *)(msg + 1);
-
-		msg_vars[0].hops = 2;
-		msg_vars[0].var_id = 255;
-		msg_vars[1].hops = 1;
-		msg_vars[1].var_id = 254;
-
-		ubyte * msg_bytecode = (ubyte *)(msg_vars + var_details);
-
-		// Debug check to make sure that we have done sane things!
-		if ((void *)(msg_bytecode + bytecode_length) - (void *)msg != packet_size)
-		{
-			printf("Failed to copy data correctly got=%d expected=%d!\n",
-				(void *)(msg_bytecode + bytecode_length) - (void *)msg,
-				packet_size);
-		}
-
-		memcpy(msg_bytecode, program_bytecode, bytecode_length);
-
-		//printf("Sent packet length %d\n", packet_size);
-
-		trickle_send(&tc);
-
+		send_example_predicate(&destination);
 
 		leds_on(LEDS_BLUE);
 
@@ -401,99 +483,111 @@ static unsigned int maximum_hop_data_request(var_elem_t const * variables, unsig
 PROCESS_THREAD(hsendProcess, ev, data)
 {
 	static struct etimer et;
-	static eval_pred_req_t const * msg;
-	static var_elem_t const * variables;
-	static ubyte const * bytecode_instructions;
 	static unsigned int max_hops = 0;
-
 	static node_data_t * all_neighbour_data = NULL;
 
 	PROCESS_EXITHANDLER(goto exit;)
 	PROCESS_BEGIN();
-
-	msg = (eval_pred_req_t const *)data;
 	
-	printf("HSEND Process Started. Waiting.\n");
+	printf("HSEND Process Started.\n");
 
 	//Wait for other nodes to initialize.
 	etimer_set(&et, 20 * CLOCK_SECOND);
 	PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-	printf("Wait finished\n");
-
-	// Pointer for bytecode variables
-	variables = (var_elem_t const *)(msg + 1);
-
-	// Create a pointer to the bytecode instructions stored in the message.
-	bytecode_instructions = (ubyte const *)(variables + msg->num_of_bytecode_var);
-
-	max_hops = maximum_hop_data_request(variables, msg->num_of_bytecode_var);
-
-	
-	// Only ask for data if the predicate needs it
-	if (max_hops != 0)
+	while (true)
 	{
-		printf("Starting request for %d hops of data...\n", max_hops);
+		printf("HSEND: Starting long wait...\n");
 
-		nhopreq_request_info(&hc, max_hops);
-	
-		// Get as much information as possible within a given time bound
-		etimer_set(&et, 120 * CLOCK_SECOND);
+		etimer_set(&et, 10 * 60 * CLOCK_SECOND);
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-		printf("Finished collecting hop data.\n");
+		printf("HSEND: Wait finished! About to ask for data!\n");
 
+		max_hops = 0;
 
-		// Generate array of all the data
-		all_neighbour_data = (node_data_t *) malloc(sizeof(node_data_t) * max_size);
-
-		unsigned int i;
-		for (i = 1; i <= max_hops ; ++i)
+		map_elem_t elem;
+		for (elem = map_first(&predicate_map); map_continue(&predicate_map, elem); elem = map_next(elem))
 		{
-			map_t * hop_map = get_hop_map(i);
+			predicate_detail_entry_t const * pe = (predicate_detail_entry_t const *)map_data(&predicate_map, elem);
 
-			unsigned int length = map_length(hop_map);
+			unsigned int local_max_hops = maximum_hop_data_request(pe->variables_details, pe->variables_details_length);
 
-			// position in all_neighbour_data
-			unsigned int count = 0;
-
-			if (length > 0)
+			if (local_max_hops > max_hops)
 			{
-				map_elem_t elem;
-				for (elem = map_first(hop_map); map_continue(hop_map, elem); elem = map_next(elem))
-				{
-					node_data_t * mapdata = (node_data_t *)map_data(hop_map, elem);
-					memcpy(&all_neighbour_data[count], mapdata, sizeof(node_data_t));
-					++count;
-				}
+				max_hops = local_max_hops;
 			}
-
-			printf("i=%d Count=%d length=%d\n", i, count, length);
 		}
-	}
+		
 
-	printf("Starting predicate evaluation with code length: %d.\n", msg->bytecode_length);
+		// Only ask for data if the predicate needs it
+		if (max_hops != 0)
+		{
+			printf("Starting request for %d hops of data...\n", max_hops);
 
+			nhopreq_request_info(&hc, max_hops);
 	
-	bool evaluation_result = evaluate_predicate(
-		bytecode_instructions, msg->bytecode_length,
-		all_neighbour_data,
-		variables, msg->num_of_bytecode_var);
+			// Get as much information as possible within a given time bound
+			etimer_set(&et, 120 * CLOCK_SECOND);
+			PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+			printf("Finished collecting hop data.\n");
 
 
-	// TODO: If predicate failed inform sink
-	// TODO: Send data back to sink
-	if (evaluation_result)
-	{
-		printf("Pred: TRUE\n");
+			// Generate array of all the data
+			all_neighbour_data = (node_data_t *) malloc(sizeof(node_data_t) * max_size);
+
+			unsigned int i;
+			for (i = 1; i <= max_hops; ++i)
+			{
+				map_t * hop_map = get_hop_map(i);
+
+				unsigned int length = map_length(hop_map);
+
+				// Position in all_neighbour_data
+				unsigned int count = 0;
+
+				if (length > 0)
+				{
+					for (elem = map_first(hop_map); map_continue(hop_map, elem); elem = map_next(elem))
+					{
+						node_data_t * mapdata = (node_data_t *)map_data(hop_map, elem);
+						memcpy(&all_neighbour_data[count], mapdata, sizeof(node_data_t));
+						++count;
+					}
+				}
+
+				printf("i=%d Count=%d length=%d\n", i, count, length);
+			}
+		}
+
+		for (elem = map_first(&predicate_map); map_continue(&predicate_map, elem); elem = map_next(elem))
+		{
+			predicate_detail_entry_t const * pe = (predicate_detail_entry_t const *)map_data(&predicate_map, elem);
+
+			printf("Starting predicate evaluation with code length: %d.\n", pe->bytecode_length);
+	
+			bool evaluation_result = evaluate_predicate(
+				pe->bytecode, pe->bytecode_length,
+				all_neighbour_data,
+				pe->variables_details, pe->variables_details_length);
+
+
+			// TODO: If predicate failed inform sink
+			// TODO: Send data back to sink
+			if (evaluation_result)
+			{
+				printf("Pred: TRUE\n");
+			}
+			else
+			{
+				printf("Pred: FAILED due to error: %s\n", error_message());
+			}
+		}
+
+		// Free the allocated neighbour data
+		free(all_neighbour_data);
 	}
-	else
-	{
-		printf("Pred: FAILED due to error: %s\n", error_message());
-	}
-
-	free(all_neighbour_data);
-	free(msg);
 
 exit:
 	printf("Exiting HSEND Process...\n");

8d3eb731f6a33a50a3e5c9d2d167416f63410195
Amit Shah <amitshah@oneuk.com>
2013-02-18 12:39:27 +0000

    Cleaned up the head file properly. Added a comment on the function defined

--- Algorithms/NeighbourAggregate/neighbour-aggregate.h
+++ Algorithms/NeighbourAggregate/neighbour-aggregate.h
@@ -1,5 +1,5 @@
-//#ifndef CS407_NEIGHBOUR_AGGREGATE
-//#define CS407_NEIGHBOUR_AGGREGATE
+#ifndef CS407_NEIGHBOUR_AGGREGATE
+#define CS407_NEIGHBOUR_AGGREGATE
 
 #include "net/rime.h"
 
@@ -9,6 +9,7 @@ typedef struct
 	rimeaddr_t second;
 } rimeaddr_pair_t;
 
+/* starts the neighbour aggregate the argument is a function that will be used by the sink to process the data */
 void start_neighbour_aggregate(void (* data_recieved_fn)(rimeaddr_pair_t *, unsigned int, int));
 
-//#endif /*CS407_NEIGHBOUR_AGGREGATE*/
+#endif /*CS407_NEIGHBOUR_AGGREGATE*/

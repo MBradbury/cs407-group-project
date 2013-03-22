dd3e9de18e17a086b7ccf86884e21c377ba7d39c
Amit Shah <amitshah@oneuk.com>
2013-03-22 19:27:11 +0000

    Updated to include rime stats, and output to a json text file

--- Tests/COOJA-Tests/getAllSensorData.js
+++ Tests/COOJA-Tests/getAllSensorData.js
@@ -1,51 +1,120 @@
-GENERATE_MSG(60000, "sleep"); //Wait for 60 secs, give the nodes a chance to start up
+//import Java Package to JavaScript
+importPackage(java.io);
+
+GENERATE_MSG(10000, "sleep"); //Wait for 60 secs, give the nodes a chance to start up
 YIELD_THEN_WAIT_UNTIL(msg.equals("sleep"));
 
-printAllIDs(); //Print all the IDs of the nodes
+TIMEOUT(1000000);
 
-//prints the information of the node
-goThroughNodes(function(m) {
-	log.log("\nMote ID = " + m.getID() + "\n");
-	log.log("Rime Address = " + m.getInterfaces().getRimeAddress().getAddressString() + "\n");
-	log.log("Temprature =  " + m.getInterfaces().get("Temperature").getTemperature() +"\n");
-	log.log("Humidity = " + m.getInterfaces().get("Humidity").getHumidity() + "\n");
-	printRimeStats(m);
-});
+//file for rimestats
+rimestats = new FileWriter("log_rimestats.txt", false);
 
-log.log("\n");
-log.testOK(); /* Report test success and quit */
+ 
+ // Use JavaScript object as an associative array
+outputs = [];
+
+file = new FileWriter("log_data.txt", false);
+
+while (true) {
+
+	if (msg.contains("Pred:")) {
+	 	//Has the output file been created.
+
+	   	log.log(time + " " + id.toString() + " " + msg + "\n");
+
+	   	round = new Object();
+	   	
+	   	round["data"] = [];
+	   	if (msg.toLowerCase().contains("true"))
+	   	{
+	   		round["pred"] = "true";
+	   	}
+	   	else
+	   	{
+	   		round["pred"] = "false";
+	   	}
+
+	   	goThroughNodes(function(m) {
+	   		temp = "" + m.getInterfaces().get("Temperature").getTemperature();
+	   		hum = "" + m.getInterfaces().get("Humidity").getHumidity();
+
+	   		json = {
+	   			"id":m.getID().toString(),
+	   			"temp":temp.toString(),
+	   			"Humidity":hum.toString(),
+	   		};
+
+	   		round["data"].push(json);
+	   	});
+
+	   	outputs.push(round);
+		message = JSON.stringify(outputs);
+		file.close();
+		file = new FileWriter("log_data.txt", false);
+	   	file.write(message + "\n");
+	 	
+	   	rimestats.close();
+		rimestats = new FileWriter("log_rimestats.txt", false);
+
+	 	//Save all the rimestats
+	 	rimes = [];
+
+	 	allmotes = sim.getMotes();
+
+		for(var i = 0; i < allmotes.length; i++) {
+			rimes.push(getRimeStats(allmotes[i]));
+		}
+
+		
+		rimestats.write(JSON.stringify(rimes) + "\n");
+	}
+ 	try{
+ 		//This is the tricky part. The Script is terminated using
+ 		// an exception. This needs to be caught.
+ 		YIELD();
+ 	} catch (e) {
+ 		//Close files.
+ 		file.close();
+		rimestats.close();
+ 		//Rethrow exception again, to end the script.
+ 		throw('test script killed');
+	}
+}
 
 //Prints the stats stored in the rimestats variable
-function printRimeStats(m)
+function getRimeStats(m)
 {
+	out = new Object();
+	out["id"] = m.getID().toString(); //ID of the node
+
 	//if the option has been enabled
 	if (m.getMemory().variableExists("rimestats")) {
 		var ar = m.getMemory().getMemorySegment(m.getMemory().getVariableAddress("rimestats"), 72);
 
 		for (var i = 0; i < ar.length; i = i + 4) {
 			switch (i / 4) {
-				case  0: log.log("tx"); break;
-				case  1: log.log("rx"); break;
-				case  2: log.log("reliabletx"); break; 
-				case  3: log.log("reliablerx"); break;
-				case  4: log.log("rexmit"); break; 
-				case  5: log.log("acktx"); break;
-				case  6: log.log("noacktx"); break; 
-				case  7: log.log("ackrx"); break;
-				case  8: log.log("timedout"); break; 
-				case  9: log.log("badackrx"); break;
-				case 10: log.log("toolong"); break; 
-				case 11: log.log("tooshort"); break; 
-				case 12: log.log("badsynch"); break; 
-				case 13: log.log("badcrc"); break;
-				case 14: log.log("contentiondrop"); break;
-				case 15: log.log("sendingdrop"); break;
-				case 16: log.log("lltx"); break;
-				case 17: log.log("llrx"); break;
+				case  0: out["tx"] = byteArrayToLong(ar.slice(i, i+3)); break;
+				case  1: out["rx"] = byteArrayToLong(ar.slice(i, i+3)); break;
+				case  2: out["reliabletx"] = byteArrayToLong(ar.slice(i, i+3)); break; 
+				case  3: out["reliablerx"] = byteArrayToLong(ar.slice(i, i+3)); break;
+				case  4: out["rexmit"] = byteArrayToLong(ar.slice(i, i+3)); break; 
+				case  5: out["acktx"] = byteArrayToLong(ar.slice(i, i+3)); break;
+				case  6: out["noacktx"] = byteArrayToLong(ar.slice(i, i+3)); break; 
+				case  7: out["ackrx"] = byteArrayToLong(ar.slice(i, i+3)); break;
+				case  8: out["timedout"] = byteArrayToLong(ar.slice(i, i+3)); break; 
+				case  9: out["badackrx"] = byteArrayToLong(ar.slice(i, i+3)); break;
+				case 10: out["toolong"] = byteArrayToLong(ar.slice(i, i+3)); break; 
+				case 11: out["tooshort"] = byteArrayToLong(ar.slice(i, i+3)); break; 
+				case 12: out["badsynch"] = byteArrayToLong(ar.slice(i, i+3)); break; 
+				case 13: out["badcrc"] = byteArrayToLong(ar.slice(i, i+3)); break;
+				case 14: out["contentiondrop"] = byteArrayToLong(ar.slice(i, i+3)); break;
+				case 15: out["sendingdrop"] = byteArrayToLong(ar.slice(i, i+3)); break;
+				case 16: out["lltx"] = byteArrayToLong(ar.slice(i, i+3)); break;
+				case 17: out["llrx"] = byteArrayToLong(ar.slice(i, i+3)); break;
 			}
-			log.log(" = " + byteArrayToLong(ar.slice(i,i+3)) + "\n");
 		}
 	}
+	return out;
 }
 
 function byteArrayToLong(/*byte[]*/byteArray) {
@@ -58,6 +127,7 @@ function byteArrayToLong(/*byte[]*/byteArray) {
 }
 
 function goThroughNodes(func) {
+
 	allmotes = sim.getMotes();
 
 	for(var i = 0; i < allmotes.length; i++) {

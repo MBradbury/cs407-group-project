9d150150701a068c215f7a1fd140e40aee648bc7
timrlaw <timrlaw@gmail.com>
2013-03-26 03:24:02 +0000

    Hoppy bugs ironed out (missions).

--- PredVis/src/predvis/Predicate.java
+++ PredVis/src/predvis/Predicate.java
@@ -5,6 +5,8 @@ import java.nio.channels.FileChannel;
 import java.nio.MappedByteBuffer;
 import java.nio.charset.Charset;
 import predvis.hoppy.Hoppy;
+import predvis.dragon.Dragon;
+import com.google.common.io.LittleEndianDataOutputStream; 
 
 /**
  *
@@ -12,7 +14,8 @@ import predvis.hoppy.Hoppy;
  */
 public class Predicate {
     //Static parser so single instance only, call ReInit() between executions.
-    private static Hoppy parser = null;
+    private static Hoppy hoppy = null;
+    private static Dragon dragon = null;
     
     //Primary state
     private String name;
@@ -53,7 +56,7 @@ public class Predicate {
             FileInputStream stream = new FileInputStream(scriptFile);
             FileChannel fc = stream.getChannel();
             MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
-            script = Charset.defaultCharset().decode(bb).toString();
+            script = Charset.forName("UTF-8").decode(bb).toString();
         } catch (IOException e) {
             //TODO
         }
@@ -80,16 +83,39 @@ public class Predicate {
     
     private void compileScript() {
         try {
-            if (parser != null) {
-                parser = new Hoppy(new FileInputStream(scriptFile));
+            String script = getScript();
+            
+            //Run Hoppy
+            InputStream in = new ByteArrayInputStream(script.getBytes(Charset.forName("UTF-8")));
+            OutputStream out = new ByteArrayOutputStream();
+            if (hoppy == null) {
+                hoppy = new Hoppy(in);
             } else {
-                parser.ReInit(new FileInputStream(scriptFile));
+                hoppy.ReInit(in);
             }
-            parser.run(new FileInputStream(scriptFile), new FileOutputStream(scriptFile.getAbsolutePath() + ".compiled"));
+            hoppy.run(out);
+            
+            String intermediate = out.toString();
+            System.out.println("Intermediate code: " + intermediate + "\n\n");
+            
+            //Run Dragon
+            in = new ByteArrayInputStream(out.toString().getBytes(Charset.forName("UTF-8")));
+            out = new ByteArrayOutputStream();
+            LittleEndianDataOutputStream leout = new LittleEndianDataOutputStream(out);
+            
+            if (dragon == null) {
+                dragon = new Dragon(in);
+            } else {
+                dragon.ReInit(in);
+            }
+            dragon.run(leout);
+            
+            //Store bytecode.
+            System.out.println("Bytecode: " + out.toString());
         } catch (FileNotFoundException e) {
             //TODO
         } catch (Exception e) {
-            //TODO
+            e.printStackTrace();
         }
     }
     
